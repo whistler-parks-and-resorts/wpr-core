52c49c54665bf8cc2cae0bd4c659f1c8dca0c7d5
Michael Bradvica <mjbradvica@protonmail.com>
2022-08-06 13:24:55 -0400

    initial security controller

---
+++ source/WPR.Presentation/Controllers/SecurityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WPR.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecurityController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUser()
+        {
+            return Ok();
+        }
+    }
+}
