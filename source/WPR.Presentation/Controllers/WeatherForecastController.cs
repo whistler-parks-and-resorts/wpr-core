d07390f5c02e80807ac2a84dfa2c35972d5bdedd
Michael Bradvica <mjbradvica@protonmail.com>
2022-08-06 12:18:26 -0400

    small fixes

--- source/WPR.Presentation/Controllers/WeatherForecastController.cs
+++ source/WPR.Presentation/Controllers/WeatherForecastController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace WPR.Presentation.Controllers
+﻿namespace WPR.Presentation.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
--- source/WPR.Presentation/Program.cs
+++ source/WPR.Presentation/Program.cs
@@ -6,16 +6,13 @@ namespace WPR.Presentation
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -25,7 +22,6 @@ namespace WPR.Presentation
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
